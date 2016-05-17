0efe6960642838330763db9337007aac96ef8237
Denis Timofeev <timofeevdv@it2g.ru>
2016-05-17 17:43:41 +0300

    Remove excess code and comments

--- directives-msg/custom-validation.js
+++ directives-msg/custom-validation.js
@@ -1,24 +1,8 @@
 var demoApp = angular.module("demoApp", ['ngMessages']);
 
-demoApp.run(['$rootScope', function ($rootScope) {
-    $rootScope.user = {
-        login: 'test@test'
-    };
-}]);
-
-demoApp.controller("TestCrl", function () {
-});
-
-demoApp.controller("NewTest", function ($scope) {
-    $scope.user = {login: 'pff@pfff'}
-});
-
 demoApp.controller("Controller",
     ['$scope', '$timeout',
         function ($scope, $timeout) {
-            var self = this;
-
-            $scope.validation = false;
             $scope.messages = [
                 {code: 'required', message: 'Поле обязательное для заполнения'},
                 {code: 'validateInn', message: 'Это не ИНН'},
@@ -31,23 +15,7 @@ demoApp.controller("Controller",
             };
 
             $scope.formSubmit = function () {
-                //$scope.validation = true;
-                //$scope.userForm.submitted = true;
                 console.log($scope.userForm.name.$error);
-
-                //console.log($scope.userForm.name.$error);
-                //console.log(userForm.lastName.$error);
-                //console.log(/[_А-ЯЁа-яё]/.test($scope.user.lastName));
-
-                /*
-                 $scope.validation = true;
-                 $timeout(function () {
-                 var x = document.getElementsByClassName("has-error");
-                 if (x.length > 0) {
-                 $scope.showTooltip(x[0].id);
-                 }
-                 }, 300);
-                 */
                 $scope.config = {
                     messages: $scope.messages
                 };
@@ -75,7 +43,6 @@ demoApp.directive('madiValidator', function ($compile) {
             };
 
             angular.forEach(scope.errors, function (error) {
-                //var validator = validators[error.code];
                 if (validator = validators[error.code]) {
                     modelCtrl.$validators[error.code] = validator;
                 }
@@ -97,9 +64,6 @@ demoApp.directive('validateRequired', function () {
         require: 'ngModel',
         scope: true,
         link: function (scope, elem, attr, controller) {
-            //scope.myVar = {invalidName: true};
-            //console.log(scope);
-
             controller.$validators.validateRequired = function (modelValue, viewValue) {
                 return modelValue != undefined && modelValue != "";
             };
@@ -110,70 +74,5 @@ demoApp.directive('validateRequired', function () {
         '</div>'
     }
 });
-demoApp.directive('validateCyrilic', function () {
-    return {
-        require: 'ngModel',
-        scope: true,
-        link: function (scope, elem, attr, controller) {
-            controller.$parsers.unshift(function (value) {
-                controller.$setValidity('validateCyrilic', /^[А-ЯЁа-яё]+$/.test(value));
-                //console.log("validateCyrilic");
-                return value;
-            });
-        }
-    }
-});
-demoApp.directive('validateInn', function () {
-    return {
-        require: 'ngModel',
-        link: function (scope, elem, attr, controller) {
-            controller.$parsers.unshift(function (value) {
-                controller.$setValidity('validateInn', /^[0-9]{10}$/.test(value));
-                return value;
-            });
-        }
-    }
-});
-demoApp.directive('uiValidationMessages', function ($compile) {
-    return {
 
-        scope: {
-            messages: '=',
-            form: '=',
-            validation: '='
-        },
-
-        replace: true,
-        templateUrl: 'messages-template.html',
-        link: function ($scope, $element, $attrs) {
-
-
-            wrapper = angular.element(document.getElementById($attrs.wrapperId));
-            wrapper.attr("ng-mouseleave", "onMouseLeave($event)");
-            wrapper.attr("ng-mouseenter", "onMouseEnter($event)");
-
-            $scope.onMouseLeave = function ($event) {
-                angular.element(document.getElementById($event.currentTarget.id)).removeClass("hovered");
-            };
-
-            $scope.onMouseEnter = function ($event) {
-                $(document.getElementsByClassName("validation-tooltip")).removeClass("hovered");
-                angular.element(document.getElementById($event.currentTarget.id)).addClass("hovered");
-            };
-
-            $compile(wrapper)($scope);
-        }
-    }
-});
-demoApp.directive('uiValidationMessage', function () {
-    return {
-        scope: {
-            message: "=",
-            msg: "@"
-        },
-        replace: true,
-        require: '^^uiValidationMessages',
-        templateUrl: 'message-template.html'
-    }
-});
 
